1e4640753e334548127422a7ebdafea9f6dbacb6
dogiaan711 <anblcc2002@gmail.com>
2023-12-27 14:04:13 +0700

    Revert "Setup Toolbar UI"
    This reverts commit 622393fc2c79c57077e4ae78979d49ed5f7787ff.

--- Assets/Scripts/UI/Slot_UI.cs
+++ Assets/Scripts/UI/Slot_UI.cs
@@ -9,8 +9,6 @@ public class Slot_UI : MonoBehaviour
     public Image itemIcon;
     public TextMeshProUGUI quantityText;
 
-    [SerializeField] private GameObject highlight;
-
     public void SetItem(Inventory.Slot slot)
     {
         if (slot != null)
@@ -26,9 +24,5 @@ public class Slot_UI : MonoBehaviour
         itemIcon.sprite = null;
         itemIcon.color = new Color(1, 1, 1, 0);
         quantityText.text = "";
-    }
-
-    public void SetHighlight(bool isOn) {
-        highlight.SetActive(isOn);
     }
 }
