e16afce436f7709d9304af85f07caebf510579b6
dogiaan711 <anblcc2002@gmail.com>
2023-12-31 20:35:17 +0700

    Fix HUDManager

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -7,7 +7,7 @@ public class HUDManager : MonoBehaviour
     public static HUDManager instance;
     // Start is called before the first frame update
     private void Start() {
-        if (instance != null  &&  instance != this) {
+        if (instance != null) {
             Destroy(this.gameObject);
             return;
         }
